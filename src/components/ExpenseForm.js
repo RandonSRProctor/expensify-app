d899862ae69b53f082c82284b98d4af95fdb9f3e
RandonSRProctor <randyproctor@gmail.com>
2021-04-09 19:29:13 -0400

    Create Expense Page in progress

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: '',
+        amount: ''
+    };
+    onDescriptionChange = (event) => {
+        const description = event.target.value;
+        this.setState(() => ({ description }));
+    }
+    onTextAreaChange = (event) => {
+        const note = event.target.value;
+        this.setState(() => ({ note }));
+    }
+    onAmountChange = (event) => {
+        const amount = event.target.value;
+        if ( amount.match(/^\d*(\.\d{0,2})?$/) ) {
+            this.setState(() => ({ amount }));
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onTextAreaChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        )
+    }
+}
